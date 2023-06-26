15e0382d6f870a06866253489078c637fd3027e7
yadong <yangyadong1900@gmail.com>
2023-06-26 09:08:42 +0800

    feat:myPromise

--- 2.promise/my-promise.ts
+++ 2.promise/my-promise.ts
@@ -3,6 +3,24 @@ type Reject = (reason?: any) => void;
 
 type onResolve = (value?: any) => any;
 type onRejected = (reason?: any) => any;
+
+function resolvePromise(x: any, promise2: any, resolve: any, reject: any) {
+  if (x === promise2) {
+    throw new TypeError('Chaining cycle detected for promise');
+  }
+  if (x instanceof MyPromise) {
+    x.then(
+      (y) => {
+        resolvePromise(y, promise2, resolve, reject);
+      },
+      (r) => {
+        reject(r);
+      }
+    );
+  } else {
+    resolve(x);
+  }
+}
 export class MyPromise {
   status: 'pending' | 'fulfilled' | 'rejected';
   value: any;
@@ -38,7 +56,6 @@ export class MyPromise {
       }
     };
 
-    const promise2 = new MyPromise((resolve, reject) => {});
     try {
       executor(resolve, reject);
     } catch (e) {
@@ -47,35 +64,103 @@ export class MyPromise {
   }
   then(onResolve?: onResolve, onRejected?: onRejected) {
     // 并不是在调用then时触发，是在调用then时收集，在executor执行完之后,在resolve/reject执行的时候触发
-    if (this.status === 'fulfilled') {
-      let x = onResolve?.(this.value);
-    }
-    if (this.status === 'rejected') {
-      let x = onRejected?.(this.reason);
-    }
-    if (this.status === 'pending') {
-      if (onResolve) {
-        this.onResolveFunc = onResolve;
+
+    let promise2 = new MyPromise((resolve, reject) => {
+      if (this.status === 'fulfilled') {
+        setTimeout(() => {
+          try {
+            let x = onResolve?.(this.value);
+            resolvePromise(x, promise2, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        });
       }
-      if (onRejected) {
-        this.onRejectFunc = onRejected;
+
+      if (this.status === 'rejected') {
+        setTimeout(() => {
+          try {
+            let x = onRejected?.(this.reason);
+            resolvePromise(x, promise2, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        });
       }
-    }
-    return new MyPromise((resolve, reject) => {
-      // resolve(x);
+
+      if (this.status === 'pending') {
+        setTimeout(() => {
+          try {
+            if (onResolve) {
+              this.onResolveFunc = () => {
+                setTimeout(() => {
+                  try {
+                    let x = onResolve?.(this.value);
+                    resolvePromise(x, promise2, resolve, reject);
+                  } catch (error) {
+                    reject(error);
+                  }
+                });
+              };
+            }
+            if (onRejected) {
+              this.onRejectFunc = () => {
+                setTimeout(() => {
+                  try {
+                    let x = onRejected?.(this.reason);
+                    resolvePromise(x, promise2, resolve, reject);
+                  } catch (error) {
+                    reject(error);
+                  }
+                });
+              };
+            }
+          } catch (error) {
+            reject(error);
+          }
+        });
+      }
     });
+
+    return promise2;
   }
+  static deferred() {
+    let defer = {} as any;
+    defer.promise = new MyPromise((resolve, reject) => {
+      defer.resolve = resolve;
+      defer.reject = reject;
+    });
+    return defer;
+  }
+
+  static resolve(value: any) {
+    if (value instanceof MyPromise) {
+      return value;
+    } else {
+      return new MyPromise((resolve, reject) => {
+        resolve(value);
+      });
+    }
+  }
+  static all(promises: MyPromise[]) {}
 }
 //
+
 const p = new MyPromise((resolve, reject) => {
   console.log('executor');
-  resolve(1);
+  setTimeout(() => {
+    resolve(1);
+  }, 1000);
 });
 const pp = p.then(
   (value) => {
     console.log('resolve value: ', value);
+    return 2;
   },
   (reason) => {
     console.log('rejected reason:', reason);
   }
 );
+const ppp = pp.then((value) => {
+  console.log('ppp', value);
+});
--- 2.promise/use-promise.ts
+++ 2.promise/use-promise.ts
@@ -1,29 +1,39 @@
-const p = new Promise((resolve, reject) => {
+// const p = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(1);
+//   }, 100);
+// });
+// const pp = p.then((value) => {
+//   console.log('s1:', value);
+//   throw new Error('error');
+// });
+
+// const ppp = pp.then(
+//   (value) => {
+//     console.log('s2 succ:', value);
+//     return 'succ';
+//   },
+//   (reason) => {
+//     console.log('s2 fail:', reason);
+//     return 'fail';
+//   }
+// );
+
+// ppp.then(
+//   (value) => {
+//     console.log('s3 succ:', value);
+//   },
+//   () => {
+//     console.log('s3 fail');
+//   }
+// );
+
+const f = () => {
   setTimeout(() => {
-    resolve(1);
-  }, 100);
+    console.log('setTimeout');
+  }, 1000);
+};
+Promise.resolve(f).then((v) => {
+  v();
+  console.log('setTimeout then:', v);
 });
-const pp = p.then((value) => {
-  console.log('s1:', value);
-  throw new Error('error');
-});
-
-const ppp = pp.then(
-  (value) => {
-    console.log('s2 succ:', value);
-    return 'succ';
-  },
-  (reason) => {
-    console.log('s2 fail:', reason);
-    return 'fail';
-  }
-);
-
-ppp.then(
-  (value) => {
-    console.log('s3 succ:', value);
-  },
-  () => {
-    console.log('s3 fail');
-  }
-);
